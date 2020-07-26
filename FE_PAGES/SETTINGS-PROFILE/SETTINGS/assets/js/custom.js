2f325946b58311492de130da05ee7de65d6e66d9
Gabriel Ilochi <gabrielilo190@gmail.com>
2020-07-25 18:40:20 -1200

    Removed a folder

--- FE_PAGES/SETTINGS-PROFILE/SETTINGS/assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.navbar-toggler'),
-      close = document.querySelector('.close'),
-      sidebar = document.querySelector('.sidebar');
-  button.addEventListener('click', (e) => {
-    sidebar.classList.toggle('show');
-  })
-  close.addEventListener('click', (e) => {
-    sidebar.classList.toggle('show');
-  })
