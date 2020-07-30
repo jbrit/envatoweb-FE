532580c114d2a29e5716ec68cf997383bc5b7846
Ajibola Ojo <41059344+jbrit@users.noreply.github.com>
2020-07-30 10:31:15 +0100

    Update campaign-step-one.js

--- FE_PAGES/assets/js/custom-js/campaign-step-one.js
+++ FE_PAGES/assets/js/custom-js/campaign-step-one.js
@@ -1,1 +1,53 @@
+const isValidField = (field, type = "text") => {
+  // Check if empty
+  if (field.value.trim() === "") return false;
+  if (type === "email")
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field.value);
+  return true;
+};
+const updateProgressBars = (percent = 10) => {
+  const progressBar = document.querySelector(".inner-progress-bar");
+  const progressPercent = document.querySelector(".progress-percent");
+  var valid = Array.from(formFields).reduce(
+    (acc, field) => acc + 0 + isValidField(field, field.type),
+    0
+  );
+  var length = formFields.length;
+  var percent = parseInt((valid / length) * percent);
+  progressBar.style.width = `${percent}%`;
+  progressPercent.innerHTML = `${percent}%`;
+};
+const inValidateField = (field) => {
+  field.classList.add("is-invalid");
+};
+const validateField = (field) => {
+  field.classList.remove("is-invalid");
+};
+const formFields = document.querySelectorAll(".form-control");
 
+// Update Progress bar on any input
+formFields.forEach((elt) => {
+  elt.addEventListener("input", (e) => {
+    updateProgressBars();
+  });
+});
+
+//On focus approve
+formFields.forEach((elt) => {
+  elt.addEventListener("focus", (e) => {
+    validateField(elt);
+  });
+});
+document
+  .querySelector("#submit-campaign-form")
+  .addEventListener("click", (e) => {
+    var invalid = false;
+    formFields.forEach((field) => {
+      if (!isValidField(field)) {
+        inValidateField(field);
+        invalid = true;
+      }
+    });
+    if (invalid) return window.scrollTo(0, 0);
+    document.querySelector("#campaign-form").submit();
+  });
